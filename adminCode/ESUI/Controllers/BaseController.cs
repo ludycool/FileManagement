3dbcdb82791a90ff733fe3003ec52d0e70837038
t1o4m5 <t1o4m5@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2016-01-02 02:58:14 +0000

    修改动态表功能提交

--- adminCode/ESUI/Controllers/BaseController.cs
+++ adminCode/ESUI/Controllers/BaseController.cs
@@ -11,6 +11,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using System.Web.UI;
+using Newtonsoft.Json;
 
 namespace ESUI.Controllers
 {
@@ -361,6 +362,53 @@ namespace ESUI.Controllers
             dt = dt.ToLocalTime();
             result = dt.ToString(FormateStr);
             return result;
+        }
+    }
+    public class JsonNetResult : JsonResult
+    {
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public Formatting Formatting { get; set; }
+        public JsonNetResult()
+        {
+            SerializerSettings = new JsonSerializerSettings();
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType =
+                !string.IsNullOrEmpty(ContentType) ? ContentType : "application/json";
+            if (ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+            if (Data != null)
+            {
+                JsonTextWriter writer = new JsonTextWriter(response.Output)
+                {
+                    Formatting = Formatting
+                };
+                JsonSerializer serializer = JsonSerializer.Create(SerializerSettings);
+                serializer.Serialize(writer, Data); writer.Flush();
+            }
+        }
+    }
+
+    public class JsonNetController : Controller
+    {
+        protected override JsonResult Json(object data, string contentType,
+                  Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            if (behavior == JsonRequestBehavior.DenyGet
+                && string.Equals(this.Request.HttpMethod, "GET",
+                                 StringComparison.OrdinalIgnoreCase))
+                //Call JsonResult to throw the same exception as JsonResult
+                return new JsonResult();
+            return new JsonNetResult()
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding
+            };
         }
     }
 
--- adminCode/ESUI/Controllers/DynamicTableController.cs
+++ adminCode/ESUI/Controllers/DynamicTableController.cs
@@ -12,6 +12,7 @@ using e3net.common.SysMode;
 using e3net.Mode;
 using e3net.Mode.RMS;
 using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
 
 namespace ESUI.Controllers
 {
@@ -99,6 +100,57 @@ namespace ESUI.Controllers
 
         
         }
+        public JsonResult ColumnEditInfo(ColumnCharts categoryTable)
+        {
+           // ColumnCharts categoryTable=new ColumnCharts();
+            bool IsAdd = false;
+            if (categoryTable != null && string.IsNullOrEmpty(categoryTable.ID))//id为空，是添加
+            {
+                IsAdd = true;
+            }
+
+
+            if (OPBiz.GetCount<ColumnChartsSet>(ColumnChartsSet.CategoryTableID.Equal(categoryTable.CategoryTableID).And(ColumnChartsSet.field.Equal(categoryTable.field))) > 0.0)
+            {
+                return Json("Nok", JsonRequestBehavior.AllowGet);
+            }
+
+            if (IsAdd)
+            {
+                categoryTable.ID = Guid.NewGuid().ToString();
+                //categoryTable.TableName_ = DateTime.Now;
+                //categoryTable.TableProperties = DateTime.Now;
+                //rol.RoleDescription = RMS_ButtonsModle.RoleDescription;
+                //rol.RoleOrder = RMS_ButtonsModle.RoleOrder;
+
+                OPBiz.Add(categoryTable);
+
+
+           
+            var catmodle = OPBiz.GetEntity(CategoryTableSet.SelectAll().Where(CategoryTableSet.ID.Equal(categoryTable.CategoryTableID)));
+
+            OPBiz.ExecuteSqlWithNonQuery("alter table " + catmodle.UserTableName + " add " + categoryTable .field+ " nvarchar(500) [null]");
+                return Json("ok", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+
+                categoryTable.WhereExpression = ColumnChartsSet.ID.Equal(categoryTable.ID);
+                //  spmodel.GroupId = GroupId;
+                if (OPBiz.Update(categoryTable) > 0)
+                {
+                    return Json("ok", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("Nok", JsonRequestBehavior.AllowGet);
+                }
+            }
+
+
+
+
+        }
         public JsonResult GetInfo(string ID)
         {
             var mql2 = CategoryTableSet.SelectAll().Where(CategoryTableSet.ID.Equal(ID));
@@ -106,6 +158,42 @@ namespace ESUI.Controllers
             //  groupsBiz.Add(rol);
             return Json(Rmodel, JsonRequestBehavior.AllowGet);
         }
+        public ActionResult ListAdd()
+        {
+          string id=  Request.QueryString["ID"];
+          ViewBag.RuteUrl = id;
+            return View();
+        }
 
+        [HttpPost]
+        public JsonResult ColumnGetList(string Condition)
+        {
+
+
+
+            //int pageIndex = Request["page"] == null ? 1 : int.Parse(Request["page"]);
+            //int pageSize = Request["rows"] == null ? 10 : int.Parse(Request["rows"]);
+            //////字段排序
+            ////String sortField = Request["sortField"];
+            ////String sortOrder = Request["sortOrder"];
+            //PageClass pc = new PageClass();
+            //pc.sys_Fields = "*";
+            //pc.sys_Key = "ID";
+            //pc.sys_PageIndex = pageIndex;
+            //pc.sys_PageSize = pageSize;
+            //pc.sys_Table = "ColumnCharts";
+            //pc.sys_Where = "1=1";
+            //pc.sys_Order = "ID";
+
+            //var list2 = OPBiz.GetPagingData(pc);
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+
+            var list2 = OPBiz.GetEntities(ColumnChartsSet.SelectAll());
+            // var mql = RMS_ButtonsSet.Id.NotEqual("");
+            dic.Add("rows", list2);
+            dic.Add("total", list2.Count);
+
+            return Json(dic, JsonRequestBehavior.AllowGet);
+        }
     }
 }
