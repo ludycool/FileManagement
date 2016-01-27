6fb44a8abdf191ed879c2b281205e7fe1f5bfc44
t1o4m5 <t1o4m5@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2016-01-27 13:51:09 +0000

    添加导出功能

---
+++ trunk/adminCode/ESUI/Controllers/ExcelController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ESUI.Models;
+using Newtonsoft.Json;
+
+namespace ESUI.Controllers
+{
+    public class ExcelController : Controller
+    {
+        //
+        // GET: /Excel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public FileResult CommonExport(string Title, string Columns, string Data)
+        {
+            var tb = JsonConvert.DeserializeObject<DataTable>(Data);
+            var Columnslist = JsonConvert.DeserializeObject<List<Column>>(Columns);
+            var workbook = new Workbook();
+            workbook.CreateSheet(new Sheet(Title, Columnslist, tb));
+            var fileStream = workbook.GetMemoryStream();
+            return File(fileStream, "application/ms-excel", string.Format("{0}.xls", Title));
+        }
+
+    }
+}
---
+++ trunk/adminCode/ESUI/Models/Column.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESUI.Models
+{
+    public class Column
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public int Width { get; set; }
+        public bool Hidden { get; set; }
+
+        public Column()
+        {
+        }
+
+        public Column(string code, string name, string dataType, int width, bool hidden = false)
+        {
+            Code = code;
+            Name = name;
+            DataType = dataType;
+            Width = width;
+            Hidden = hidden;
+        }
+    }
+}
