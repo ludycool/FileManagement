9b157f5aab2b913d958d34c507628032ebd67e4c
t1o4m5 <t1o4m5@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2016-03-03 13:52:00 +0000

    添加操作类

---
+++ trunk/adminCode/e3net.IDAL/DynamicTable/IMainAssociationDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DefaultConnection;
+using e3net.Mode;
+
+namespace e3net.IDAL.DynamicTable
+{
+    public interface IMainAssociationDao : IBaseDao<MainAssociation>
+    {
+    }
+}
