e352514221aa54da0a67723e3f2fd363355cff08
ludycool <ludycool@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2016-03-27 02:29:42 +0000

    dataGrid选择行，保存数据

--- trunk/adminCode/ESUI/Content/js/CommonFunction.js
+++ trunk/adminCode/ESUI/Content/js/CommonFunction.js
@@ -297,6 +297,81 @@ var DataBaseFunction = {
     }
 
 }
+
+
+var DataGridCheck = {//DataGrid选择操作 保存数据 
+    Idfield: 'Id',//Id列名
+    dataGridSelecttor: '#dg',//datagrid选择器
+    checkedItems: [],//选择的数据
+    addcheckItem: function () {//添加项
+        var row = $(DataGridCheck.dataGridSelecttor).datagrid('getChecked');
+        for (var i = 0; i < row.length; i++) {
+            if (DataGridCheck.findCheckedItem(row[i][DataGridCheck.Idfield]) == -1) {
+                DataGridCheck.checkedItems.push(row[i]);
+            }
+        }
+    }, removeSingleItem: function (rowIndex, rowData) {//移除单项
+        var k = DataGridCheck.findCheckedItem(rowData[DataGridCheck.Idfield]);
+        if (k != -1) {
+            DataGridCheck.checkedItems.splice(k, 1);
+        }
+    },
+    removeAllItem: function (row) {//移除所有
+        for (var i = 0; i < row.length; i++) {
+            var k = DataGridCheck.findCheckedItem(row[i][DataGridCheck.Idfield]);
+            if (k != -1) {
+                DataGridCheck.checkedItems.splice(k, 1);
+            }
+        }
+    },
+    SetcheckItem: function (dataGridSelecttor, Idfield) {//设置选中
+        for (var i = 0; i < DataGridCheck.checkedItems.length; i++) {
+            $(DataGridCheck.dataGridSelecttor).datagrid('selectRecord', DataGridCheck.checkedItems[i][DataGridCheck.Idfield]); //根据id选中行 
+        }
+    },
+    findCheckedItem: function (IdValue) {//是否存在
+        for (var i = 0; i < DataGridCheck.checkedItems.length; i++) {
+            if (DataGridCheck.checkedItems[i][DataGridCheck.Idfield] == IdValue) return i;
+        }
+        return -1;
+    }
+
+};
+//html 控件操作
+var controlSet = {
+    RadioSetSelect: function (formId, name, value) {//radio选中
+        var _o = document.forms[formId][name];
+        for (i = 0; i < _o.length; i++) {
+            if (_o[i].value == String(value))
+            { _o[i].checked = true; }
+            else {
+                _o[i].checked = false;
+            }
+        }
+    },
+    RadioBindSelect: function (formId, name, value) {//radio绑定控件
+        var _o = document.forms[formId][name];
+        for (i = 0; i < _o.length; i++) {
+            if (_o[i].value == String(value))
+            { _o[i].checked = true; }
+            else {
+                _o[i].checked = false;
+            }
+        }
+    },
+    RadioGetSelectValue: function (formId, name) {//radio获取选中值
+        var _o = document.forms[formId][name];
+        for (i = 0; i < _o.length; i++) {
+            if (_o[i].checked) {
+                return _o[i].value;
+                break;
+            }
+        }
+    }
+
+}
+
+
 //设置宽高自适应w  h 宽高偏移量
 function PanelAutoResizeWH(PanelSelect, w, h) {
     var winWidth = GetwinWidth();
@@ -381,39 +456,7 @@ function GetwinWidth() {
 
 }
 
-//html 控件操作
-var controlSet = {
-    RadioSetSelect: function (formId, name, value) {//radio选中
-        var _o = document.forms[formId][name];
-        for (i = 0; i < _o.length; i++) {
-            if (_o[i].value == String(value))
-            { _o[i].checked = true; }
-            else {
-                _o[i].checked = false;
-            }
-        }
-    },
-    RadioBindSelect: function (formId, name, value) {//radio绑定控件
-        var _o = document.forms[formId][name];
-        for (i = 0; i < _o.length; i++) {
-            if (_o[i].value == String(value))
-            { _o[i].checked = true; }
-            else {
-                _o[i].checked = false;
-            }
-        }
-    },
-    RadioGetSelectValue: function (formId, name) {//radio获取选中值
-        var _o = document.forms[formId][name];
-        for (i = 0; i < _o.length; i++) {
-            if (_o[i].checked) {
-                return _o[i].value;
-                break;
-            }
-        }
-    }
 
-}
 
 //JS获取地址栏参数的方法（超级简单）
 function GetQueryString(name) {
