a9fa46b45c2e004508690b6dc4af5e22e355e0b9
t1o4m5 <t1o4m5@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2016-10-02 01:58:35 +0000

    修复问题提交

---
+++ trunk/adminCode/e3net.tools/EnumConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace e3net.tools
+{
+  public  class EnumConfig
+    {
+      [Description("签收状态")]
+      public enum ArchiveType
+      {
+          [Description("代签收")] 
+          GenerationSign,
+          [Description("已签收")]
+          HaveSign,
+      }
+    }
+}
