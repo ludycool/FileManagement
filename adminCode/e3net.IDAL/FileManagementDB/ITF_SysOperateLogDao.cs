8985430966d09af860f712e9cecee0f8d62541fd
janhuang2100 <janhuang2100@6691f10d-5a41-4d96-bb6d-82b828143a1d>
2017-01-06 13:35:07 +0000

    操作日志-1

---
+++ adminCode/e3net.IDAL/FileManagementDB/ITF_SysOperateLogDao.cs
@@ -0,0 +1,12 @@
+﻿using e3net.Mode.FileManagementDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace e3net.IDAL
+{
+    public interface ITF_SysOperateLogDao : IBaseDao<TF_SysOperateLog>
+    {
+    }
+}
